47d38588bf2ac6e44aaaa05510bfaa62283a92fa
Kit Double <kit.double@sydney.edu.au>
2026-03-28 10:37:25 +1100

    Fix Tower of Hanoi rings being pushed offscreen by double translation

--- task-gallery/src/components/showcase/Previews.tsx
+++ task-gallery/src/components/showcase/Previews.tsx
@@ -436,8 +436,6 @@ export function TowerOfHanoiPreview() {
           return (
             <motion.rect
               key={`${disc}`}
-              x={x}
-              y={y}
               animate={{ x, y }}
               transition={{ type: "spring", stiffness: 200, damping: 20 }}
               width={w}
